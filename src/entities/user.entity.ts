87486e4866bda90813a599509ddd1c5330be324b
Libin Biji <libinbiji@Libins-MacBook-Air.local>
2025-06-30 14:39:59 +0530

    feat: user roles added.

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Document } from "./document.entity";
+import { UserRole } from "src/enums/user.enum";
 
 @Entity()
 export class User {
@@ -12,18 +13,25 @@ export class User {
     @Column()
     lastname: string
 
-    @Column({nullable: true})
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.SERVICE_REQUESTER,
+    })
+    role: UserRole;
+
+    @Column({ nullable: true })
     otp: number
 
     @Column()
     password: string
 
-    @Column({default: true})
+    @Column({ default: true })
     is_available: boolean
 
-    @Column({unique: true})
+    @Column({ unique: true })
     contact: string
 
-    @OneToMany(() => Document, (document) => document.user, {nullable: true, cascade: true})
+    @OneToMany(() => Document, (document) => document.user, { nullable: true, cascade: true })
     documents: Document[]
 }
---
+++ src/enums/user.enum.ts
@@ -0,0 +1,6 @@
+export enum UserRole {
+    "SERVICE_PROVIDER" = "SP",
+    "SERVICE_REQUESTER" = "SR",
+    "TECHNICIAN" = "T",
+    "ADMIN" = "ADM"
+}
